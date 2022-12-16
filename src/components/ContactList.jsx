bdc7f2642373435e1e1d762f716f2941991a7d89
ali mokhtari <alimokhtari910@yahoo.com>
2022-12-16 10:53:40 +0330

    Extract Contact Components

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -4,27 +4,38 @@ import { AiOutlineDelete } from 'react-icons/ai';
 const ContactList = ({ contacts, deleteContactHandler }) => {
   return (
     <div className="contact-container">
-      {contacts.map((contact) =>  (
-        <div key={contact.id} className="contact">
-          <figure>
-            <img src={contact.img} alt="avatar" className="avatar-img" />
-          </figure>
-          <div className="contact-desc">
-            <p className="contact-name">{contact.name}</p>
-            <p className="contact-email">{contact.email}</p>
-          </div>
-          <div>
-            <button
-              className="delete-contact-btn"
-              onClick={() => deleteContactHandler(contact.id)}
-            >
-              <AiOutlineDelete />
-            </button>
-          </div>
-        </div>
+      {contacts.map((contact) => (
+        <Contact
+          contact={contact}
+          deleteContactHandler={deleteContactHandler}
+          key={contact.id}
+        />
       ))}
     </div>
   );
 };
 
 export default ContactList;
+
+// Extract Contact Components
+const Contact = ({ contact, deleteContactHandler }) => {
+  return (
+    <div className="contact">
+      <figure>
+        <img src={contact.img} alt="avatar" className="avatar-img" />
+      </figure>
+      <div className="contact-desc">
+        <p className="contact-name">{contact.name}</p>
+        <p className="contact-email">{contact.email}</p>
+      </div>
+      <div>
+        <button
+          className="delete-contact-btn"
+          onClick={() => deleteContactHandler(contact.id)}
+        >
+          <AiOutlineDelete />
+        </button>
+      </div>
+    </div>
+  );
+};
